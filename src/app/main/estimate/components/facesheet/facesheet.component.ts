531b390e279e26f84439357849d9cd8c12744e75
porapot <porapot2@gmail.com>
2020-02-20 17:39:48 +0700

    dragfile

--- src/app/main/estimate/components/facesheet/facesheet.component.ts
+++ src/app/main/estimate/components/facesheet/facesheet.component.ts
@@ -24,27 +24,13 @@ export class FacesheetComponent implements OnInit {
   deleteFile(index: number) {
     this.files.splice(index, 1);
   }
-  uploadFilesSimulator(index: number) {
-    setTimeout(() => {
-      if (index === this.files.length) {
-        return;
-      } else {
-        const progressInterval = setInterval(() => {
-          if (this.files[index].progress === 100) {
-            clearInterval(progressInterval);
-            this.uploadFilesSimulator(index + 1);
-          } else {
-            this.files[index].progress += 5;
-          }
-        }, 200);
-      }
-    }, 1000);
-  }
+  
   prepareFilesList(files: Array<any>) {
     for (const item of files) {
       item.progress = 0;
       this.files.push(item);
     }
-    this.uploadFilesSimulator(0);
+    
   }
+
 }
